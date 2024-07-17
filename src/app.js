2d02ebc590eba94113b51e6dd09da5c677cfcd2b
Mateus <mateusmpi@gmail.com>
2024-07-17 17:35:41 -0300

    fix: import paths test 6

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import router from "./src/routes/indexRouter.js";
+import routes from "./src/routes/indexRouter";
 
 dotenv.config();
 
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // ROTAS AQUI
-app.use(router);
+app.use(routes);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Rodando na porta ${port}`));
